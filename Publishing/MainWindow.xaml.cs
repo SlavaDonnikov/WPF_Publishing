4b10cb97307d86812747323604aea93400eac920
Viachaslau Donnikov <slavadonnikov@gmail.com>
2018-04-07 17:01:10 +0300

    Add slider for media player. Can jump to video player positions using slider. Create the interaction logic of video player menu buttons and background application music button.

--- Publishing/MainWindow.xaml.cs
+++ Publishing/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Shapes;
 using System.Media;
 using WPFPdfViewer;
 using System.Windows.Threading;
+using System.Windows.Media.Animation;
 
 namespace Publishing
 {
@@ -27,7 +28,7 @@ namespace Publishing
     public partial class MainWindow : Window
     {
         #region Global Variables
-        private MediaPlayer ApplicationMusicTheme; // add application music theme, which plays all time        
+        private MediaPlayer ApplicationMusicTheme; // add application music theme, which plays all time           
         #endregion
 
         #region MainWindow()
@@ -40,6 +41,7 @@ namespace Publishing
             ApplicationBackgroundMusic();
 
             Grid_HomePage.Visibility = Visibility.Visible;
+            
         }
         #endregion
 
@@ -88,8 +90,15 @@ namespace Publishing
 
         private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
-            Dispatcher.Invoke(() => ApplicationRealTime.Content = DateTime.Now.ToLongTimeString());
-
+            try
+            {
+                Dispatcher.Invoke(() => ApplicationRealTime.Content = DateTime.Now.ToLongTimeString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error : " + ex.Message + ", " + ex.Source);
+            }
+           
             // In ru-RU culture : ApplicationRealTime.Content = DateTime.Now.ToLongTimeString() + " " + DateTime.Now.AddHours(12).ToString("tt", CultureInfo.InvariantCulture)
         }
         #endregion
@@ -136,17 +145,19 @@ namespace Publishing
         }
         #endregion
 
-        // Создать ползунок громкости, ползунок контента, выбор кол-ва секунд "вперед" "назад"
-        #region VideoPlayer Buttons and Settings
-        private void SetVideoPlayerTimer()
+        // Создать ползунок громкости, ползунок контента, выбор кол-ва секунд "вперед" "назад"        
+        #region VideoPlayer
+
+        #region Video Time Timer
+        private void SetVideoPlayerTimeTimer()
         {
-            DispatcherTimer VidepPlayerTimer = new DispatcherTimer();
-            VidepPlayerTimer.Interval = TimeSpan.FromSeconds(0);
-            VidepPlayerTimer.Tick += VideoPlayerTimer_Tick;
-            VidepPlayerTimer.Start();
+            DispatcherTimer VidepPlayerTimeTimer = new DispatcherTimer();
+            VidepPlayerTimeTimer.Interval = TimeSpan.FromSeconds(0);
+            VidepPlayerTimeTimer.Tick += VideoPlayerTimeTimer_Tick;
+            VidepPlayerTimeTimer.Start();
         }
 
-        private void VideoPlayerTimer_Tick(object sender, EventArgs e)
+        private void VideoPlayerTimeTimer_Tick(object sender, EventArgs e)
         {
             if(VideoPlayer.Source != null)
             {
@@ -155,40 +166,63 @@ namespace Publishing
                         String.Format($"{VideoPlayer.Position.ToString(@"mm\:ss")} / {VideoPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss")}");
             }            
         }
-        
+        #endregion
+
         private void SetVideoPlayer()       // Main Set Method
         {            
             VideoPlayer_Play_Button.Visibility = VideoPlayer_Stop_Button.Visibility = VideoPlayer_Forward_Button.Visibility = VideoPlayer_Rewind_Button.Visibility = 
-                VideoPlayerVolumeOffOnButton.Visibility = VideoPlayer_Time_Label.Visibility = VideoPlayer_LoadNewVideo_Button.Visibility = Visibility.Collapsed;
+                VideoPlayerVolumeOffOnButton.Visibility = VideoPlayer_Time_Label.Visibility = VideoPlayer_LoadNewVideo_Button.Visibility = VideoPlayer_Slider_StackPanel.Visibility = Visibility.Collapsed;
             VideoPlayer_Play_Button.IsEnabled = VideoPlayer_Stop_Button.IsEnabled = VideoPlayer_Forward_Button.IsEnabled = VideoPlayer_Rewind_Button.IsEnabled =
-                VideoPlayerVolumeOffOnButton.IsEnabled = VideoPlayer_Time_Label.IsEnabled = VideoPlayer_LoadNewVideo_Button.IsEnabled = false;
+                VideoPlayerVolumeOffOnButton.IsEnabled = VideoPlayer_Time_Label.IsEnabled = VideoPlayer_LoadNewVideo_Button.IsEnabled = VideoPlayer_Slider_StackPanel.IsEnabled = false;
 
-            SetVideoPlayerTimer();
+            SetVideoPlayerTimeTimer();
         }
 
         private void VideoPlayer_Initiate_Button_Click(object sender, RoutedEventArgs e)        // Initial Button
         {
             VideoPlayer_Play_Button.Visibility = VideoPlayer_Stop_Button.Visibility = VideoPlayer_Forward_Button.Visibility = VideoPlayer_Rewind_Button.Visibility =
-                VideoPlayerVolumeOffOnButton.Visibility = VideoPlayer_Time_Label.Visibility = VideoPlayer_LoadNewVideo_Button.Visibility = Visibility.Visible;
+                VideoPlayerVolumeOffOnButton.Visibility = VideoPlayer_Time_Label.Visibility = VideoPlayer_LoadNewVideo_Button.Visibility = VideoPlayer_Slider_StackPanel.Visibility = Visibility.Visible;
             VideoPlayer_Play_Button.IsEnabled = VideoPlayer_Stop_Button.IsEnabled = VideoPlayer_Forward_Button.IsEnabled = VideoPlayer_Rewind_Button.IsEnabled =
-                VideoPlayerVolumeOffOnButton.IsEnabled = VideoPlayer_Time_Label.IsEnabled = VideoPlayer_LoadNewVideo_Button.IsEnabled = true;
-
-            VideoPlayerBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F5F5F5"));
-
-            ApplicationMusicTheme.Pause();
-
-            VideoPlayer.Play();
-
+                VideoPlayerVolumeOffOnButton.IsEnabled = VideoPlayer_Time_Label.IsEnabled = VideoPlayer_LoadNewVideo_Button.IsEnabled = VideoPlayer_Slider_StackPanel.IsEnabled = true;
+            
             VideoPlayer_Initiate_Button.Visibility = Visibility.Collapsed;
 
+            VideoPlayer.Play();
             VideoPlayer_Play_Button.Content = FindResource("Pause");
+
+            ApplicationMusicTheme.Pause();
+            ApplicationVolumeOffOnButton.Content = FindResource("ApplicationVolumeOff");
+
+            VideoPlayerBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F5F5F5"));
         }
 
-        private void VideoPlayer_LoadNewVideo_Button_Click(object sender, RoutedEventArgs e)
-        {
-            VideoPlayer_Stop_Button_Click(new object(), new RoutedEventArgs());
-            ApplicationMusicTheme.Pause();
+        private void VideoPlayer_MediaEnded(object sender, RoutedEventArgs e) { VideoPlayer_Stop_Button_Click(new object(), new RoutedEventArgs()); }
 
+        private void VideoPlayer_MediaFailed(object sender, ExceptionRoutedEventArgs e) { MessageBox.Show("Media filed! Smth was wrong and occured an error."); }
+
+        // Добавить: движение слайдера зависящее от хода воспроизведения видео. Возможность перемотать видео не перетягивая слайдер, а щелкнув в любубю его часть
+        // Slider https://stackoverflow.com/questions/10208959/binding-mediaelement-to-slider-position-in-wpf
+        // https://github.com/ButchersBoy/MaterialDesignInXamlToolkit/blob/master/MaterialDesignThemes.Wpf/Themes/MaterialDesignTheme.Slider.xaml
+        // http://www.cyberforum.ru/wpf-silverlight/thread421374.html
+        #region Slider Timer
+        private void VideoPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            // Here also add a volume and video time span bindings
+            
+            VideoPlayer_Slider.Maximum = VideoPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                        
+        }
+        private void VideoPlayer_Slider_LostMouseCapture(object sender, MouseEventArgs e)
+        {
+            TimeSpan time = new TimeSpan(0, 0, Convert.ToInt32(Math.Round(VideoPlayer_Slider.Value))); //отлавливаем позицию на которую нужно перемотать трек
+            VideoPlayer.Position = time; //устанавливаем новую позицию для трека
+        }
+       
+        #endregion
+
+        #region Menu Buttons
+        private void VideoPlayer_LoadNewVideo_Button_Click(object sender, RoutedEventArgs e)
+        {                        
             string formats = "All Videos Files |*.dat; *.wmv; *.3g2; *.3gp; *.3gp2; *.3gpp; *.amv; *.asf; *.ts; *.tts; *.vob; *.vro; *.webm; " +
                                                 " *.avi; *.bin; *.cue; *.divx; *.dv; *.flv; *.gxf; *.iso; *.m1v; *.m2v; *.m2t; *.m2ts; *.m4v; " +
                                                 " *.mkv; *.mov; *.mp2; *.mp2v; *.mp4; *.mp4v; *.mpa; *.mpe; *.mpeg; *.mpeg1; *.mpeg2; *.mpeg4; " +
@@ -202,11 +236,15 @@ namespace Publishing
             };
             if (openFileDialog.ShowDialog() == true)
             {
+                VideoPlayer_Stop_Button_Click(new object(), new RoutedEventArgs());
+                ApplicationMusicTheme.Pause();
+                ApplicationVolumeOffOnButton.Content = FindResource("ApplicationVolumeOff");
+
                 try
                 {
                     VideoPlayer.Source = new Uri(openFileDialog.FileName, UriKind.Absolute);
                     VideoPlayer_Play_Button.Content = FindResource("Pause");
-                    VideoPlayer.Play();
+                    VideoPlayer.Play();                    
                 }
                 catch (Exception ex)
                 {
@@ -214,10 +252,12 @@ namespace Publishing
                 }
             }
         }
-
+        
         private void VideoPlayer_Play_Button_Click(object sender, RoutedEventArgs e)
-        {
-            if(VideoPlayer.Source == null)
+        {            
+            VideoPlayerBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F5F5F5"));
+
+            if (VideoPlayer.Source == null)
             {
                 try
                 {
@@ -232,21 +272,23 @@ namespace Publishing
             if (VideoPlayer_Play_Button.Content == FindResource("Play"))
             {
                 ApplicationMusicTheme.Pause();
+                ApplicationVolumeOffOnButton.Content = FindResource("ApplicationVolumeOff");
+
                 VideoPlayer.Play();
                 VideoPlayer_Play_Button.Content = FindResource("Pause");
-
             }
             else
             {
                 VideoPlayer.Pause();
                 VideoPlayer_Play_Button.Content = FindResource("Play");
             }
-
         }
         
         private void VideoPlayer_Stop_Button_Click(object sender, RoutedEventArgs e)
         {
             ApplicationMusicTheme.Play();
+            ApplicationVolumeOffOnButton.Content = FindResource("ApplicationVolumeOn");
+
             VideoPlayer.Stop();
             VideoPlayer.ClearValue(MediaElement.SourceProperty);
 
@@ -256,16 +298,20 @@ namespace Publishing
 
             VideoPlayerVolumeOffOnButton.Content = FindResource("PlayerVolumeOn");
             VideoPlayer.Volume = 1;
+
+            VideoPlayer_Slider.Value = 0; // сброс позиции слайдера
         }
 
         private void VideoPlayer_Forward_Button_Click(object sender, RoutedEventArgs e)
         {
             VideoPlayer.Position += TimeSpan.FromSeconds(10);
+            VideoPlayer_Slider.Value = VideoPlayer.Position.TotalSeconds; // вроде работает
         }
 
         private void VideoPlayer_Rewind_Button_Click(object sender, RoutedEventArgs e)
         {
             VideoPlayer.Position -= TimeSpan.FromSeconds(10);
+            VideoPlayer_Slider.Value = VideoPlayer.Position.TotalSeconds; // вроде работает
         }
 
         private void VideoPlayerVolumeOffOnButton_Click(object sender, RoutedEventArgs e)
@@ -280,7 +326,10 @@ namespace Publishing
                 VideoPlayer.Volume = 1;
                 VideoPlayerVolumeOffOnButton.Content = FindResource("PlayerVolumeOn");
             }
-        }        
+        }
+        #endregion
+
+
         #endregion
 
         #region "Power" button (close application)_Click Event.
@@ -458,6 +507,11 @@ namespace Publishing
                 MessageBox.Show(ex.Message, ex.Source);
             }
         }
+
+
+
+
+
         #endregion
 
 
@@ -485,6 +539,7 @@ namespace Publishing
         //    }
         //}       
         #endregion
+
         
     }
 }
